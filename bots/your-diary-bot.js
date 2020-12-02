cf4a691cbaaf907bdf431d8c2874bb295ace29b9
Alexandre <49409332+AlexandreMagnesium@users.noreply.github.com>
2020-12-03 00:22:39 +0200

    MVP

--- bots/your-diary-bot.js
+++ bots/your-diary-bot.js
@@ -8,10 +8,31 @@ const month = ["січня", "лютого", "беререзня", "квітня
 
 
 const startBot = () => {
+
+	bot.start( ctx => ctx.reply(
+`Привіт, ${ctx.from.first_name}!
+Я – бот розроблений @my_Iover, щоб зручно вести щоденик.
+Підключи мене до свого щоденника, як це вказано в інструкції нижче.`))
 	
-	bot.command("a", (ctx) => {
-		ctx.reply("AAAAAA!!");
+	bot.command("work", (ctx) => {
+		ctx.reply("YES!!! -3!!! uuhhhuuuuu!!!!");
 	});
+
+
+	cron.schedule('0 7 * * *', () => {
+  		bot.telegram.sendMessage(process.env.CHANNEL,
+`<b>${day[new Date().getDay()]}, ${new Date().getDate()} ${month[new Date().getMonth()]} ${new Date().getFullYear()}</b>
+
+
+
+<b>П'ять речей, зроблених за день, якими я пишаюсь:</b>
+
+<b>1. </b>
+<b>2. </b>
+<b>3. </b>
+<b>4. </b> 
+<b>5. </b>`, {parse_mode : "HTML"})
+})
 
 	bot.launch();
 }
