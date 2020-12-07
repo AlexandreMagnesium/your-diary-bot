3b7308e9d0a480fc78072f94721f202c2646e74c
Alexandre <49409332+AlexandreMagnesium@users.noreply.github.com>
2020-12-07 16:01:04 +0200

    Update your-diary-bot.js

--- bots/your-diary-bot.js
+++ bots/your-diary-bot.js
@@ -1,4 +1,4 @@
-const Telegraf= require('telegraf');
+const Telegraf = require('telegraf');
 const bot = new Telegraf(process.env.TOKEN);
 
 const cron = require('node-cron')
@@ -6,32 +6,39 @@ const cron = require('node-cron')
 const day = ["Нд", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"]
 const month = ["січня", "лютого", "беререзня", "квітня", "травня", "червня", "липня", "серпня", "вересня", "жовтня", "листопада", "грудня"]
 
+let post_template = `<b>${day[new Date().getDay()]}, ${new Date().getDate()} ${month[new Date().getMonth()]} ${new Date().getFullYear()}</b>\n
+	\n
+	\n
+	\n
+	<b>П'ять речей, зроблених за день, якими я пишаюсь:</b>\n
+	\n
+	<b>1. </b>\n
+	<b>2. </b>\n
+	<b>3. </b>\n
+	<b>4. </b>\n
+	<b>5. </b>`
 
 const startBot = () => {
 
 	bot.start( ctx => ctx.reply(
-`Привіт, ${ctx.from.first_name}!
-Я – бот розроблений @my_Iover, щоб зручно вести щоденик.
-Підключи мене до свого щоденника, як це вказано в інструкції нижче.`))
+		`Привіт, ${ctx.from.first_name}!\n
+		Я – бот розроблений @MATU_KOTIB, щоб зручно вести щоденик.\n
+		Підключи мене до свого щоденника, як це вказано в інструкції нижче.`
+	))
+	
 	
 	bot.command("work", (ctx) => {
-		ctx.reply("YES!!! -3!!! uuhhhuuuuu!!!!");
-	});
+		ctx.reply("YES!!! -3!!! uuhhhuuuuu!!!!")
+	})
+
+
+	bot.command("new_post", (ctx) => {
+		bot.telegram.sendMessage(process.env.CHANNEL, post_template, {parse_mode : "HTML"})
+	})
 
 
 	cron.schedule('0 7 * * *', () => {
-  		bot.telegram.sendMessage(process.env.CHANNEL,
-`<b>${day[new Date().getDay()]}, ${new Date().getDate()} ${month[new Date().getMonth()]} ${new Date().getFullYear()}</b>
-
-
-
-<b>П'ять речей, зроблених за день, якими я пишаюсь:</b>
-
-<b>1. </b>
-<b>2. </b>
-<b>3. </b>
-<b>4. </b> 
-<b>5. </b>`, {parse_mode : "HTML"})
+		bot.telegram.sendMessage(process.env.CHANNEL, post_template, {parse_mode : "HTML"})
 })
 
 	bot.launch();
